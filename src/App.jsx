5c66828c283d34d68869404b5bc842280d83d5eb
Cellyn <celyhernandez94@gmail.com>
2024-11-19 03:39:36 -0600

    subiendo la Single Page Application basada en spotify y usando react

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Main from './pages/main/Main'
+import Login from './components/login/Login'
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className="w-full min-h-screen bg-zinc-950 text-neutral-400 flex flex-col p-2 overflow-hidden">
+          {/*aqui iran las rutas*/}
+          <Routes>
+            <Route exact path='/' element={<Main />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  )
+}
+
+export default App
